2bef0ea989fdf02ba06d695048bc46d0e9bfb7a3
Jym-lab <jym9809@gmail.com>
2023-10-27 16:03:57 +0900

    feat: CarClass

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,28 @@
 package racingcar;
 
+
+class Car {
+    private String name;
+    private int position;
+
+    public Car(String name){
+        this.name = name;
+        position = 0;
+    }
+    public void move(int randomNumber){
+        if (randomNumber > 3){
+            position++;
+        }
+    }
+    public String getName(){
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
